fd2e9e30ca31e03e6bbbf33af8cce4c6fc4ae9ed
puneetjyot <puneetjyot.khurana@sjsu.edu>
2020-11-25 15:33:12 -0800

    add backend

---
+++ termproject/src/main/java/edu/sjsu/cmpe275/termproject/TermprojectApplication.java
@@ -0,0 +1,13 @@
+package edu.sjsu.cmpe275.termproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TermprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TermprojectApplication.class, args);
+	}
+
+}
---
+++ termproject/src/test/java/edu/sjsu/cmpe275/termproject/TermprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.sjsu.cmpe275.termproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TermprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
