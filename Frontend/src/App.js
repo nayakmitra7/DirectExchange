926572edee2321218d4fe0d6e481a3f0147f555b
puneetjyot <puneetjyot.khurana@sjsu.edu>
2020-11-26 02:24:01 -0800

    cors issue solved

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
-import Main from './components/Main';
-import { Router } from 'react-router-dom';
-import { history } from './js/helper/history';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React, { Component } from "react";
+import Main from "./components/Main";
+import { Router } from "react-router-dom";
+import { history } from "./js/helper/history";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 //App Component
 class App extends Component {
   render() {
     return (
       <Router history={history}>
         <div>
-        <ToastContainer autoClose={3000} />
-          <Main/>
+          <ToastContainer autoClose={3000} />
+          <Main />
         </div>
       </Router>
     );
