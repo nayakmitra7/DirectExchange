2bc89cdbf3b2beae7493629a411fd544c414a45d
puneetjyot <puneetjyot.khurana@sjsu.edu>
2020-11-27 00:49:04 -0800

    resolved confilcts

--- Backend/src/main/java/com/sjsu/cmpe275/term/service/offer/OfferService.java
+++ Backend/src/main/java/com/sjsu/cmpe275/term/service/offer/OfferService.java
@@ -1,20 +1,14 @@
 package com.sjsu.cmpe275.term.service.offer;
 
 
-<<<<<<< HEAD
 import java.util.List;
-=======
-import java.util.Optional;
->>>>>>> ff1737ccb8b9d6d979af0c343dcb1e29d636cec7
+
 
 import com.sjsu.cmpe275.term.models.Offer;
 
 public interface OfferService {
 	public Offer postOffer(Offer offer);
-<<<<<<< HEAD
 
 	public List<Offer> getOffer();
-=======
 	public Offer getOfferById(Long offerId);
->>>>>>> ff1737ccb8b9d6d979af0c343dcb1e29d636cec7
 }
--- Backend/src/main/java/com/sjsu/cmpe275/term/service/offer/OfferServiceImpl.java
+++ Backend/src/main/java/com/sjsu/cmpe275/term/service/offer/OfferServiceImpl.java
@@ -1,11 +1,9 @@
 package com.sjsu.cmpe275.term.service.offer;
 
 
-<<<<<<< HEAD
+
 import java.util.List;
-=======
-import java.util.Optional;
->>>>>>> ff1737ccb8b9d6d979af0c343dcb1e29d636cec7
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,21 +17,21 @@ public class OfferServiceImpl implements OfferService {
 	@Autowired
 	OfferRepository offerRepository;
 
+	
+
+	@Override
+    public List<Offer> getOffer() {
+		
+		return offerRepository.findAll();
+	}
+	@Override
+	public Offer getOfferById(Long offerId) {
+		// TODO Auto-generated method stub
+		return offerRepository.findById(offerId).orElse(null);
+	}
 	@Override
 	public Offer postOffer(Offer offer) {
 		// TODO Auto-generated method stub
 		return offerRepository.save(offer);
-	}
-
-	@Override
-<<<<<<< HEAD
-	public List<Offer> getOffer() {
-		
-		return offerRepository.findAll();
-=======
-	public Offer getOfferById(Long offerId) {
-		// TODO Auto-generated method stub
-		return offerRepository.findById(offerId).orElse(null);
->>>>>>> ff1737ccb8b9d6d979af0c343dcb1e29d636cec7
 	}
 }
