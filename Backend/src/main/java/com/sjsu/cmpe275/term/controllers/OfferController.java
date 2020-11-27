0f03214b8d87c04d66fbdfa48a49820f6c9ea258
puneetjyot <puneetjyot.khurana@sjsu.edu>
2020-11-27 00:45:17 -0800

    added get offer api

--- Backend/src/main/java/com/sjsu/cmpe275/term/controllers/OfferController.java
+++ Backend/src/main/java/com/sjsu/cmpe275/term/controllers/OfferController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -36,7 +37,8 @@ public class OfferController {
 	@ResponseBody
 	public ResponseEntity<ResponseDTO> postOffer(@RequestBody OfferDto offerDTO) {
 		try {
-			Offer offer = new Offer(offerDTO.getSourceCountry(), offerDTO.getSourceCurrency(),offerDTO.getAmount(),offerDTO.getAmountInUSD(),offerDTO.getDestinationCountry(),offerDTO.getDestinationCurrency(),offerDTO.getExpirationDate(),offerDTO.getCounterOfferAllowed(),offerDTO.getSplitOfferAllowed());
+			//Offer offer = new Offer(offerDTO.getSourceCountry(), offerDTO.getSourceCurrency(),offerDTO.getAmount(),offerDTO.getAmountInUSD(),offerDTO.getDestinationCountry(),offerDTO.getDestinationCurrency(),offerDTO.getExpirationDate(),offerDTO.getCounterOfferAllowed(),offerDTO.getSplitOfferAllowed(), offerDTO.getUserId());
+			Offer offer = objectMapper.convertValue(offerDTO, Offer.class);
 			Offer savedOffer = offerService.postOffer(offer);
 			ResponseDTO responseDTO = new ResponseDTO(200, HttpStatus.OK, "Your offer has beeen succesfully posted!");
 			return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.OK);
@@ -61,7 +63,6 @@ public class OfferController {
 		}
 	
 	}
-	
 	
 
 	
