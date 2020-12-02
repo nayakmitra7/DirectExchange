dd51268e49a24e25fde97eb19a261be3101f3342
Mitra Nayak <nayakmitra7@gmail.com>
2020-12-02 01:03:26 -0800

    Accept split

--- Frontend/src/components/AutoMatching/autoMatching.js
+++ Frontend/src/components/AutoMatching/autoMatching.js
@@ -28,30 +28,33 @@ class AutoMatching extends Component {
             offerId: parseInt(localStorage.getItem("autoMatchId")),
             offerSrcAmount: 0,
             modalShow: false,
+            modalShowSplit: false,
             sourceAmountChange: 0,
             destinationAmount: 0,
             spinner: false,
-            offer2:''
+            offer2: '',
+            offer1: '',
+            list: []
         }
-        this.modify = this.modify.bind(this);
+        this.acceptSplit = this.acceptSplit.bind(this);
         this.accept = this.accept.bind(this);
         this.handleClose = this.handleClose.bind(this);
         this.handleOpen = this.handleOpen.bind(this);
         this.setStateSourceAmount = this.setStateSourceAmount.bind(this);
         this.acceptModal = this.acceptModal.bind(this);
-
+        this.handleOpenSplit = this.handleOpenSplit.bind(this);
     }
     componentDidMount() {
         axios.get(address + '/offerMatching/single/' + this.state.offerId).then((response) => {
-            console.log(response.data)
             if (response.status == 200) {
-                this.setState({ singleOfferList: response.data, offerExists: 1, offerSrcAmount: response.data.offer.amountInSrc, sourceAmountChange: response.data.offer.amountInSrc })
+                this.setState({ singleOfferList: response.data, offerExists: 1, offerSrcAmount: response.data.offer.amountInSrc, sourceAmountChange: response.data.offer.amountInSrc, offer1: response.data.offer })
             }
         }).catch(error => {
             toast.error("Internal error has occured", { position: 'top-center', autoClose: false })
         })
         axios.get(address + '/offerMatching/split/' + this.state.offerId).then((response) => {
             if (response.status == 200) {
+                console.log(response.data)
                 this.setState({ splitOfferList: response.data, offerExists: 1 })
             }
         }).catch(error => {
@@ -59,17 +62,40 @@ class AutoMatching extends Component {
         })
     }
     handleClose = () => {
-        this.setState({ modalShow: false })
+        this.setState({ modalShow: false, modalShowSplit: false })
     };
     handleOpen = (offer2) => {
-        console.log()
-        this.setState({ modalShow: true, destinationAmount: offer2.amountInDes, offer2:offer2 })
+        this.setState({ modalShow: true, destinationAmount: offer2.amountInDes, offer2: offer2 })
+    };
+    handleOpenSplit = (list) => {
+        this.setState({ modalShowSplit: true, list: list })
     };
     setStateSourceAmount = (event) => {
         this.setState({ sourceAmountChange: event.target.value })
     }
-    modify = (id) => {
+    acceptSplit = (list) => {
+        this.handleClose()
+        this.setState({ spinner: true });
 
+        let data = { isSplit: true, offerId1: list[0].id, offerId2: list[1].id, offerId3: list[2].id, offerUserId1: list[0].userId, offerUserId2: list[1].userId, offerUserId3: list[2].userId }
+        console.log(data)
+        axios.post(address + "/threePartyTransaction", data).then((response) => {
+            if (response.status == 200) {
+                this.setState({ spinner: false })
+                toast.success("Your offer has now entered in transaction mode");
+                axios.get(address + '/offerMatching/split/' + this.state.offerId).then((response) => {
+                    console.log(response.data)
+                    if (response.status == 200) {
+                        this.setState({ splitOfferList: [], singleOfferList: [] })
+                    }
+                }).catch(error => {
+                    toast.error("Internal error has occured", { position: 'top-center', autoClose: false })
+                })
+
+            }
+        }).catch((error) => {
+            toast.error("Internal Error Occured");
+        })
     }
     accept = (offer2) => {
         this.handleClose()
@@ -83,7 +109,7 @@ class AutoMatching extends Component {
                 axios.get(address + '/offerMatching/single/' + this.state.offerId).then((response) => {
                     console.log(response.data)
                     if (response.status == 200) {
-                        this.setState({ singleOfferList: response.data, offerExists: 1, offerSrcAmount: response.data.offer.amountInSrc, sourceAmountChange: response.data.offer.amountInSrc })
+                        this.setState({ singleOfferList: [], splitOfferList: [] })
                     }
                 }).catch(error => {
                     toast.error("Internal error has occured", { position: 'top-center', autoClose: false })
@@ -95,17 +121,48 @@ class AutoMatching extends Component {
         })
     }
     acceptModal = () => {
-        
+
         if (this.state.destinationAmount == this.state.sourceAmountChange) {
             const params = {
                 id: this.state.offerId,
                 amountInSrc: this.state.sourceAmountChange
-        }
-            axios.put(address+'/offer?id='+this.state.offerId+'&amountInSrc='+this.state.sourceAmountChange, params).then((response) =>{
-                if(response.status == 200){
+            }
+            axios.put(address + '/offer?id=' + this.state.offerId + '&amountInSrc=' + this.state.sourceAmountChange, params).then((response) => {
+                if (response.status == 200) {
                     this.accept(this.state.offer2);
                 }
-            }).catch(() =>{
+            }).catch(() => {
+                toast.error("Internal Error Occurred")
+            })
+        } else {
+            toast.error("The offers must match")
+        }
+    }
+    acceptModalSplit = () => {
+        let destAmount = 1 * this.state.list[0].amountInDes;
+        let srcAmount1 = 1 * this.state.list[1].amountInSrc;
+        let srcAmount2 = 1 * this.state.list[2].amountInSrc;
+
+        let srcAmount = 1 * this.state.list[0].amountInSrc;
+        let desAmount1 = 1 * this.state.list[1].amountInDes;
+        let desAmount2 = 1 * this.state.list[2].amountInDes;
+        console.log(srcAmount1 + " " + this.state.sourceAmountChange + " = " + destAmount);
+        console.log(srcAmount2 + " " + this.state.sourceAmountChange + " = " + destAmount);
+        console.log(srcAmount1 + " " + srcAmount2 + " = " + this.state.sourceAmountChange);
+        console.log(desAmount1 + " " + this.state.sourceAmountChange + " = " + srcAmount);
+        console.log(desAmount2 + " " + this.state.sourceAmountChange + " = " + srcAmount);
+        console.log(desAmount1 + " " + desAmount2 + " = " + this.state.sourceAmountChange);
+
+        if (srcAmount1 + 1 * this.state.sourceAmountChange == destAmount || srcAmount2 + 1 * this.state.sourceAmountChange == destAmount || srcAmount1 + srcAmount2 == this.state.sourceAmountChange || desAmount1 + desAmount2 == this.state.sourceAmountChange || desAmount1 + this.state.sourceAmountChange == srcAmount || desAmount2 + this.state.sourceAmountChange == srcAmount) {
+            const params = {
+                id: this.state.offerId,
+                amountInSrc: this.state.sourceAmountChange
+            }
+            axios.put(address + '/offer?id=' + this.state.offerId + '&amountInSrc=' + this.state.sourceAmountChange, params).then((response) => {
+                if (response.status == 200) {
+                    this.acceptSplit(this.state.list);
+                }
+            }).catch(() => {
                 toast.error("Internal Error Occurred")
             })
         } else {
@@ -121,11 +178,17 @@ class AutoMatching extends Component {
         let standard = "";
         let i = 0;
         this.state.splitOfferList.forEach((element) => {
-            let val = "" + i;
+            let val = i + "";
             i++;
             let innerSplit = [];
+            let sumSource = 0;
+            let sumDest = 0;
+            let list = [];
+            list.push(element.offer);
             element.matchingOffer.forEach((offer) => {
-
+                sumSource += offer.amountInSrc;
+                sumDest += offer.amountInDes;
+                list.push(offer);
                 innerSplit.push(<ListGroup.Item variant={offer.userId == this.state.userId ? warning : standard}>
                     <Row className="header-bold-auto-matching">
                         <Col>Offer ID</Col>
@@ -159,24 +222,29 @@ class AutoMatching extends Component {
                     <Row className="header-bold-auto-matching">
                         <Col>Offer ID</Col>
                         <Col>Username</Col>
-                        <Col>Country(des)</Col>
-                        <Col>Currency(des)</Col>
-                        <Col>Amount(des)</Col>
-                        <Col>Amount(src)</Col>
+
+
                         <Col>Country(src)</Col>
                         <Col>Currency(src)</Col>
+                        <Col>Amount(src)</Col>
+                        <Col>Amount(des)</Col>
+                        <Col>Currency(des)</Col>
+
+                        <Col>Country(des)</Col>
+
                         <Col>Exp Date</Col>
 
                     </Row>
                     <Row>
                         <Col>#{element.offer.id}</Col>
                         <Col>{element.offer.nickname}</Col>
-                        <Col>{element.offer.destinationCountry}</Col>
-                        <Col>{element.offer.destinationCurrency}</Col>
-                        <Col>{element.offer.amountInDes}</Col>
-                        <Col>{element.offer.amountInSrc}</Col>
+
                         <Col>{element.offer.sourceCountry}</Col>
                         <Col>{element.offer.sourceCurrency}</Col>
+                        <Col>{element.offer.amountInSrc}</Col>
+                        <Col>{element.offer.amountInDes}</Col>
+                        <Col>{element.offer.destinationCurrency}</Col>
+                        <Col>{element.offer.destinationCountry}</Col>
                         <Col>{element.offer.expirationDate}</Col>
 
                     </Row>
@@ -184,16 +252,36 @@ class AutoMatching extends Component {
                 </ListGroup.Item>
 
             )
-            innerSplit.push(<Row className="margin-top-1-auto-matching">
-                <Col md="5"></Col>
-                <Col md="1.5">
-                    <Button variant="success" size="sm">Accept Offer</Button>
-                </Col>
+            if (element.offer.id == this.state.userId) {
+                innerSplit.push(<Row className="margin-top-1-auto-matching">
+                    <Col md="5"></Col>
+                    {sumDest == element.offer.amountInSrc && <Col md="1.5">
+                        <Button variant="success" size="sm" onClick={() => this.acceptSplit(list)}>Accept Offer</Button>
+                    </Col>}
+                    {sumDest != element.offer.amountInSrc && <Col md="1.5">
+                        <Button variant="danger" size="sm" onClick={() => { this.handleOpenSplit(list) }}>Modify My Offer</Button>
+                    </Col>}
 
-                <Col md="2">
-                    <Button size="sm"> Counter Offer</Button>
-                </Col>
-            </Row>)
+                    <Col md="2">
+                        <Button size="sm"> Counter Offer</Button>
+                    </Col>
+                </Row>)
+            } else {
+                innerSplit.push(<Row className="margin-top-1-auto-matching">
+                    <Col md="5"></Col>
+                    {sumSource == element.offer.amountInDes && <Col md="1.5">
+                        <Button variant="success" size="sm" onClick={() => this.acceptSplit(list)}>Accept Offer</Button>
+                    </Col>}
+                    {sumSource != element.offer.amountInDes && <Col md="1.5">
+                        <Button variant="danger" size="sm" onClick={() => { this.handleOpenSplit(list) }}>Modify My Offer</Button>
+                    </Col>}
+
+                    <Col md="2">
+                        <Button size="sm"> Counter Offer</Button>
+                    </Col>
+                </Row>)
+            }
+
             splitMatches.push(
                 <Card>
                     <Accordion.Toggle as={Card.Header} eventKey={val} className="gray-auto-matching header-bold-auto-matching">
@@ -370,6 +458,52 @@ class AutoMatching extends Component {
 
                     </Modal.Body>
 
+                </Modal>
+                <Modal show={this.state.modalShowSplit} onHide={this.handleOpen} size="lg">
+                    <Modal.Header>
+                        <Modal.Title>Offer Modification</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Form>
+                            <Row>
+                                <Col>
+                                    <Row>
+                                        <Col className="header-bold-auto-matching">Offer ID</Col>
+                                        {this.state.offerExists && <Col>#{this.state.singleOfferList.offer.id}</Col>}
+                                    </Row>
+                                    <Row>
+                                        <Col className="header-bold-auto-matching">Username</Col>
+                                        {this.state.offerExists && <Col>{this.state.singleOfferList.offer.nickname}</Col>}
+                                    </Row>
+                                    <Row>
+                                        <Col className="header-bold-auto-matching">Country(des)</Col>
+                                        {this.state.offerExists && <Col>{this.state.singleOfferList.offer.destinationCountry}</Col>}
+                                    </Row>
+                                    <Row>
+                                        <Col className="header-bold-auto-matching">Amount(des)</Col>
+                                        {this.state.offerExists && <Col>{this.state.singleOfferList.offer.amountInDes} {this.state.singleOfferList.offer.destinationCurrency}</Col>}
+                                    </Row>
+                                    <Row>
+                                        <Col className="header-bold-auto-matching">Amount(src)</Col>
+                                        {this.state.offerExists && <Col><input value={this.state.sourceAmountChange} onChange={this.setStateSourceAmount} />  {this.state.singleOfferList.offer.sourceCurrency}</Col>}
+                                    </Row>
+                                    <Row>
+                                        <Col className="header-bold-auto-matching">Country(src)</Col>
+                                        {this.state.offerExists && <Col>{this.state.singleOfferList.offer.sourceCountry}</Col>}
+                                    </Row>
+                                    <Row>
+                                        <Col className="header-bold-auto-matching">Exp Date</Col>
+                                        {this.state.offerExists && <Col>{this.state.singleOfferList.offer.expirationDate}</Col>}
+                                    </Row>
+
+                                </Col>
+                            </Row>
+                        </Form>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={this.handleClose}>Close</Button>
+                        <Button variant="success" onClick={this.acceptModalSplit} >Accept offer</Button>
+                    </Modal.Footer>
                 </Modal>
             </div>
         )
