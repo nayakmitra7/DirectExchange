8a6d99e3c45da3c0ada88b70fa8940b4e170b507
puneetjyot <puneetjyot.khurana@sjsu.edu>
2020-12-12 19:21:05 -0800

    added timestap in model

---
+++ Backend/src/main/java/com/sjsu/cmpe275/term/models/BaseEntity.java
@@ -0,0 +1,36 @@
+package com.sjsu.cmpe275.term.models;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import lombok.Data;
+
+@Data
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Column(updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+    
+    
+}
