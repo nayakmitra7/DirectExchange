0f5dc1cf4f510782c807d7c7bd9b57173125ff9c
kenamehta <kenamehta1995@gmail.com>
2020-12-14 04:56:07 -0800

    intermediate for transaction report

--- Backend/src/main/java/com/sjsu/cmpe275/term/repository/OfferRepository.java
+++ Backend/src/main/java/com/sjsu/cmpe275/term/repository/OfferRepository.java
@@ -2,7 +2,6 @@ package com.sjsu.cmpe275.term.repository;
 
 import java.util.Date;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,15 +11,23 @@ import org.springframework.stereotype.Repository;
 import com.sjsu.cmpe275.term.models.Offer;
 
 @Repository
-public interface OfferRepository extends JpaRepository<Offer, Long>{
+public interface OfferRepository extends JpaRepository<Offer, Long> {
 	@Query("Select o1 from Offer o1 where o1.expirationDate >= :expirationDate and o1.userId != :userId and o1.offerStatus=1 ")
-	List<Offer> getOffer(@Param("userId") Long userId,@Param("expirationDate") Date expirationDate);
-	
-	@Query("Select o1 from Offer o1 where o1.expirationDate >= :expirationDate and o1.userId = :userId and o1.offerStatus=:offerStatus")
-	List<Offer> getOwnOfferById(@Param("userId")Long userId, @Param("expirationDate") Date expirationDate,@Param("offerStatus")int offerStatus);
-	
-	@Query("Select o1 from Offer o1 where o1.userId = :userId and (o1.expirationDate < :expirationDate or o1.offerStatus in (:offerExpired,:offerFulFiled))")
-	List<Offer> getCloseOfferById(@Param("userId")Long userId, @Param("expirationDate") Date expirationDate,@Param("offerExpired") int offerExpired,@Param("offerFulFiled") int offerFulFiled);
+	List<Offer> getOffer(@Param("userId") Long userId, @Param("expirationDate") Date expirationDate);
 
-	
+	@Query("Select o1 from Offer o1 where o1.expirationDate >= :expirationDate and o1.userId = :userId and o1.offerStatus=:offerStatus")
+	List<Offer> getOwnOfferById(@Param("userId") Long userId, @Param("expirationDate") Date expirationDate,
+			@Param("offerStatus") int offerStatus);
+
+	@Query("Select o1 from Offer o1 where o1.userId = :userId and (o1.expirationDate < :expirationDate or o1.offerStatus in (:offerExpired,:offerFulFiled))")
+	List<Offer> getCloseOfferById(@Param("userId") Long userId, @Param("expirationDate") Date expirationDate,
+			@Param("offerExpired") int offerExpired, @Param("offerFulFiled") int offerFulFiled);
+
+	@Query("Select o1 from Offer o1 where o1.userId = :userId and o1.offerStatus=:offerStatus")
+	List<Offer> getClosedTransactionOffers(@Param("userId") Long userId, @Param("offerStatus") int offerStatus);
+
+	@Query("Select o1 from Offer o1 where o1.userId = :userId and o1.offerStatus=:offerStatus")
+	List<Offer> getClosedTransactionOffersByMonth(@Param("userId") Long userId, @Param("offerStatus") int offerStatus,
+			@Param("month") int month);
+
 }
